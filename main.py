d359dd140b1954052e5dc21d30c9fead76b5d78f
Morty <olaniran.abd@gmail.com>
2025-01-20 11:55:16 +0100

    feat: path params

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 from fastapi import FastAPI
-
+from enum import Enum
 app =  FastAPI()
 
 
-@app.get("/", description="Main root of Fast API")
-async def root():
+@app.get("/")
+async def welcome():
     return {"message": "Hello World"}
+
+@app.get("/users")
+async def items(): 
+    return { "message" : "Users lists" }
+
+@app.get("/users/me")
+async def current_user(): 
+    return { "message": "current user " }
+
+@app.get("/users/{id}")
+async def item(id : int):
+    return {"message": f'user {id}'}
+
+class FoodEum(str , Enum): 
+    fruits = "fruits"
+    vegetables ="vegetables"
+    dairy = "dairy"
+
+@app.get("/foods/{name}")
+
+async def get_food(name: FoodEum):
+
+    if name == FoodEum.vegetables: 
+        return { "name": name ,  "message": "You are healthy " }
+    elif name.value == "fruits" : 
+        return { "message": f"You are eating {name.value} " } 
+    else :
+        return { "name": name , "message": " treat you well" }
